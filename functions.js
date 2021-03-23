2fde26ea1d8b046948cf0afd2ad3b1f5ec711f17
Hernan Di Tano <hditano@gmail.com>
2021-03-23 01:47:21 -0300

    exercises completed

--- functions.js
+++ functions.js
@@ -112,3 +112,41 @@ ask(
 //   function() { alert("You agreed."); },
 //   function() { alert("You canceled the execution."); } 
 // );
+
+/* Write a function called add7 that takes one number and returns that number + 7.
+Write a function called multiply that takes 2 numbers and returns their product.
+Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+Write a function called lastLetter that takes a string and returns the very last letter of that string:
+lastLetter("abcd") should return "d" */
+
+// function add7 (number) {
+//   return number + 7;
+// }
+
+// let add7 = (number) => number + 7;
+
+// console.log(add7(2));
+
+
+// function multiply (num1, num2) {
+//   return num1 * num2;
+// }
+
+// let multi = (num1, num2) => num1 * num2;
+
+// console.log(multiply(2,2));
+// console.log(multi(2,2));
+
+// function Capitalize(string) {
+//    return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
+// console.log(Capitalize('hernan'));
+
+// function lastLetter(string) {
+//   return string.slice(-1);
+// }
+
+// let lastLetter = (string) => string.slice(-1);
+
+// console.log(lastLetter('abcd'));
